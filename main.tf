24a71d2b2663a6f5f0666be294e37d2c65ee48e3
SVR Kalluru <raghuk.vit@gmail.com>
2024-04-09 07:13:02 +0530

    Enable Frontend

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# module "frontend" {
-#   depends_on = [module.backend]
-#
-#   source        = "./modules/app"
-#   instance_type = var.instance_type
-#   component     = "frontend"
-#   env           = var.env
-#   zone_id       = var.zone_id
-#   vault_token   = var.vault_token
-#   subnets       = module.vpc.frontend_subnets
-#   vpc_id        = module.vpc.vpc_id
-# }
+module "frontend" {
+  depends_on = [module.backend]
+
+  source        = "./modules/app"
+  instance_type = var.instance_type
+  component     = "frontend"
+  env           = var.env
+  zone_id       = var.zone_id
+  vault_token   = var.vault_token
+  subnets       = module.vpc.frontend_subnets
+  vpc_id        = module.vpc.vpc_id
+}
 
 module "backend" {
   depends_on = [module.mysql]
